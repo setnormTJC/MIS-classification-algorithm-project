9f0ebd91efbe84c1f6727b6bf93d8cd9b83c610d
snorm <snorm@SETHHP>
2026-03-22 12:18:31 -0500

    Is the second CSV file there yet?!

--- main.py
+++ main.py
@@ -4,59 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
-# 1. FETCH THE GOLD STANDARD DATA (UCI Statlog German Credit)
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data-numeric"
-# This version is space-delimited and has no header row
-df = pd.read_csv(url, sep=r'\s+', header=None)
+#fetch data from the web
+# url = "TBC"
+df = pd.read_csv("Loan approval data.csv"); #replace with URL later
 
 # Force Pandas to show all columns
 pd.set_option('display.max_columns', None)
-
-column_names = [
-    'Checking_Account_Status',
-    'Loan_Duration(Months)',
-    'Credit_History',
-    'Purpose',
-    'Credit_Amount',
-    'Savings_Account',
-    'Employment_Since(Months)',
-    'Installment_Rate',
-    'Personal_Status',
-    'Other_Debtors',
-    'Residence_Since(months)',
-    'Property',
-    'Age(Years)',
-    'Other_Installment_Plans',
-    'Housing',
-    'Num_Existing_Credits',
-    'Job_Status',
-    'Num_People_Liable',
-    'Telephone',
-    'Foreign_Worker',
-    'Extra_1',
-    'Extra_2',
-    'Extra_3',
-    'Extra_4',
-    'Bank_Decision'
-]
-
-#apply meaningful column headers
-df.columns = column_names;
-
-
-
+pd.set_option('display.max_rows', None)
+rowWidth = 200; #will vary depending on IDE and zoom level
+pd.set_option('display.width', rowWidth)
 
 
 print(df.head());
 
-# Apply mappings to a copy so we don't break the numeric data for the model
-export_df = df.copy()
 
-checking_map = {1: '< 0 DM', 2: '0-200 DM', 3: '> 200 DM', 4: 'No Account'}
-
-
-filename = "German_Credit_Data_1994.csv";
-export_df.to_csv(filename, index=False);
-
-#asdfasdfasdf
-#asdfasdfasdf
