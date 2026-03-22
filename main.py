09801d8babb3ee00bdc09acb233a99c0a0b06d31
snorm <snorm@SETHHP>
2026-03-22 12:05:49 -0500

    Added simple loan approval data CSV file

--- main.py
+++ main.py
@@ -40,14 +40,19 @@ column_names = [
     'Bank_Decision'
 ]
 
-#
+#apply meaningful column headers
 df.columns = column_names;
+
+
+
+
 
 print(df.head());
 
 # Apply mappings to a copy so we don't break the numeric data for the model
 export_df = df.copy()
 
+checking_map = {1: '< 0 DM', 2: '0-200 DM', 3: '> 200 DM', 4: 'No Account'}
 
 
 filename = "German_Credit_Data_1994.csv";
@@ -55,38 +60,3 @@ export_df.to_csv(filename, index=False);
 
 #asdfasdfasdf
 #asdfasdfasdf
-
-
-# 2. SELECT OUR "NUTS AND BOLTS" FEATURES
-# Column 0: Duration (Months)
-# Column 1: Credit Amount (DM)
-# Column 2: Installment Rate (% of income)
-# Column 24: Target (1=Good, 2=Bad)
-
-# X = df[[0, 1, 2]]
-# y = df[24].map({1: 1, 2: 0}) # Map to 1 (Approved/Good) and 0 (Denied/Bad)
-
-
-
-# # 3. SCALE THE DATA (To fix the "Scale Trap")
-# scaler = StandardScaler()
-# X_scaled = scaler.fit_transform(X)
-#
-# # 4. TRAIN THE LOGISTIC REGRESSION (Gradient Descent)
-# model = LogisticRegression(solver='liblinear')
-# model.fit(X_scaled, y)
-#
-# # 5. RESULTS & VISUALIZATION
-# weights = model.coef_[0]
-# feature_names = ['Duration (Mos)', 'Credit Amount', 'Installment Rate']
-#
-# print(f"Model Accuracy on Real-World Data: {model.score(X_scaled, y)*100:.2f}%")
-# print("-" * 30)
-# for name, w in zip(feature_names, weights):
-#     print(f"{name:18} | Weight: {w:.4f}")
-#
-# plt.figure(figsize=(8, 5))
-# plt.bar(feature_names, weights, color=['darkblue', 'darkred', 'orange'])
-# plt.axhline(0, color='black', linewidth=1)
-# plt.title('UCI German Credit Data: Feature Weights')
-# plt.show()
